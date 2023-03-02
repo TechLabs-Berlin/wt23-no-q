eeee2bcb7a648a6655a36a2c0323cddfc65adbc5
SeannaMarie <seanna.coleman@wsu.edu>
2023-03-02 13:35:53 +0100

    removed old code

--- src/components/paymentform/paymentform.js
+++ src/components/paymentform/paymentform.js
@@ -1,18 +1,3 @@
-/* ffc keyboard command vs sfc */
-/* 
-function Payment() {
-  const link = "https://www.paypal.com/de/signin";
-  return (
-    <>
-      <h1>Payment Page</h1>
-      <a href={link}>Pay with Paypal</a>
-    </>
-  );
-}
-
-export default Payment;
- */
-
 import React, { useState } from "react";
 import "./paymentform.css";
 
