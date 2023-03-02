625ececf69efe6a9efd7677d9320529a0851a6e6
SeannaMarie <seanna.coleman@wsu.edu>
2023-03-02 13:21:11 +0100

    updated payment.js it had duplicate code

--- src/pages/payment/payment.js
+++ src/pages/payment/payment.js
@@ -11,20 +11,3 @@ function Payment() {
     </>
   );
 }
-
-export default Payment;
-import React from "react";
-import PaymentForm from "../../components/paymentform/paymentform";
-
-function Payment() {
-  const link = "https://www.paypal.com/de/signin";
-  return (
-    <>
-      <h1>Payment Page</h1>
-      <a href={link}>Pay with Paypal</a>
-      <PaymentForm />
-    </>
-  );
-}
-
-export default Payment;
