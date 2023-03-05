939e4b0b9249865e6d5c291bd823b2cc4f8f0417
PhaedraMaya <phaedra.sarpaki@gmail.com>
2023-03-05 15:39:44 +0200

    homepage with clickable image to direct in login form

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,6 +1,42 @@
 
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { Link } from 'react-router-dom';
 
-export default function Home(){
-  return(
-      <h1>Home</h1>)
+
+
+
+export default function Home() {
+  return (
+    <div>
+
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: '100vh' }}
+      >
+        <h1>Home</h1>
+        <Link to='./profile.js'>
+          <Box
+            component="img"
+            sx={{
+              height: 500,
+              width: 500,
+              maxHeight: { xs: 350, md: 500 },
+              maxWidth: { xs: 350, md: 500 },
+              '&:hover': {
+                backgroundColor: 'black',
+                opacity: [0.9, 0.8, 0.7],
+              },
+            }}
+            alt="logo"
+            src="./images/logo.png"
+          />
+        </Link>
+      </Grid>
+
+    </div>)
 }
