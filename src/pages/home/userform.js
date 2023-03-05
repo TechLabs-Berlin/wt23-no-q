2249bf59035132510ca9b869d693541f941a841b
PhaedraMaya <phaedra.sarpaki@gmail.com>
2023-03-06 00:56:45 +0200

    creating the user's form and start to store data through fetch

--- src/pages/home/userform.js
+++ src/pages/home/userform.js
@@ -1,17 +1,30 @@
 import './userform.css';
+import { useState } from 'react';
 
-export default function userForm() {
+const UserForm = () => {
+    const [name, setName] = useState('');
+    const [number, setNumbers] = useState('');
+    const [drink, setDrink] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const user = { name, number, drink };
+        console.log(user);
+    }
 
     return (
         <div className="create">
             <h1>User Form</h1>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <label>Name:</label>
-                <input type="text" required />
+                <input type="text" placeholder="What's your name?" value={name}
+                    onChange={(e) => setName(e.target.value)} required />
                 <label>Number of people:</label>
-                <input type="number" min="1" max="30" required />
+                <input type="number" min="1" max="30" placeholder="How many are you?"
+                    value={number}
+                    onChange={(e) => setNumbers(e.target.value)} />
                 <label>Favorite drink</label>
-                <select>
+                <select value={drink} onChange={(e) => setDrink(e.target.value)}>
                     <option value="">--Please select an option</option>
                     <option value="whiskey">Whiskey</option>
                     <option value="gin">Gin</option>
@@ -22,7 +35,13 @@ export default function userForm() {
 
                 </select>
                 <button>Get in Q!</button>
+                <p>{name}</p>
+                <p>{number}</p>
+                <p>{drink}</p>
+
             </form>
         </div>
     )
 }
+
+export default UserForm;
