05508798eb25fdd19d3d16e0dc57fe2c9f0ba9f8
PhaedraMaya <phaedra.sarpaki@gmail.com>
2023-03-06 18:53:27 +0200

    stylizing files and correcting css files

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,7 +1,4 @@
 import React from 'react';
-// import { flexbox } from '@mui/system';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
 import { Link } from 'react-router-dom';
 import './home.css';
 
@@ -12,25 +9,15 @@ export default function Home() {
   return (
     <div>
 
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justify="flex-start"
-        style={{ minHeight: '100vh' }}
-      >
-        <Grid>
-          <Link to='/userform'>
-            <img className="logo" src="./images/newlogo_bigger.png" alt="logo" />
+      <div className='container'>
 
-          </Link>
-        </Grid>
-        <Grid>
+        <Link to='/userform'>
+          <img className='logo-img' src='./images/newlogo_bigger.png' alt='logo' />
+        </Link>
 
-          <h1 className='app-name'>NO-Q</h1>
-        </Grid>
-      </Grid>
+        <h1 className='name-app'>NO-Q</h1>
+
+      </div>
 
     </div>)
 }
