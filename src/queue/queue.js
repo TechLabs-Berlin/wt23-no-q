1bb59e4a1cf81d33071db589dba0a4e017ecc9d8
SeannaMarie <seanna.coleman@wsu.edu>
2023-03-21 20:25:56 +0100

    colons weren't saved for some reason?

--- src/queue/queue.js
+++ src/queue/queue.js
@@ -1,33 +1,32 @@
 // Priority Queue
 
-
 export default class Queue {
-    constructor() {
-        // create empty array for users
-        this.users = [];
-    }
-    // add user in the queue
-    enqueue(user) {
-        this.users.push(user);
-    }
+  constructor() {
+    // create empty array for users
+    this.users = [];
+  }
+  // add user in the queue
+  enqueue(user) {
+    this.users.push(user);
+  }
 
-    // remove user from the queue
-    dequeue() {
-        // it removes the last element of the array and returns it
-        return this.users.shift();
-    }
+  // remove user from the queue
+  dequeue() {
+    // it removes the last element of the array and returns it
+    return this.users.shift();
+  }
 
-    peek() {
-        // it will just show you the first element of the array
-        return this.users[0]
-    }
-    // get the size of the array
+  peek() {
+    // it will just show you the first element of the array
+    return this.users[0];
+  }
+  // get the size of the array
 
-    getSize() {
-        return this.users.length
-    }
+  getSize() {
+    return this.users.length;
+  }
 
-    isEmpty() {
-        return this.getSize() === 0;
-    }
+  isEmpty() {
+    return this.getSize() === 0;
+  }
 }
