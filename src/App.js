08b850279f00625bfa49eedcd4bb4d7e1c444e17
SeannaMarie <seanna.coleman@wsu.edu>
2023-04-09 00:24:31 +0200

    Created drink rating component.
    issues with node modules tried deleting and then installing.

--- src/App.js
+++ src/App.js
@@ -16,9 +16,7 @@ import data from "./data";
 import Product from "./pages/drinks/product";
 import InQueue from "./pages/home/inQueue";
 import Form from "./pages/home/Form";
-
-
-
+import DrinkRating from "./components/drinkRating/drinkRating";
 
 function App() {
   // when user get's in the queue and log in
@@ -29,28 +27,28 @@ function App() {
   const [cartItems, setCartItems] = useState([]);
   // use history to redirect in home page
   const onAdd = (product) => {
-    // exist is a variable that check cart items and try to find an item that its id is equal to product id. 
+    // exist is a variable that check cart items and try to find an item that its id is equal to product id.
     const exist = cartItems.find((x) => x.id === product.id);
     // then we have to update the quantity in it
     if (exist) {
-      // if it exists we will add one in the quantity ({...exist, qty: exist.qty + 1 }) 
+      // if it exists we will add one in the quantity ({...exist, qty: exist.qty + 1 })
       // otherwise we'll keep it as it is (:x)
-      setCartItems(cartItems.map((x) => x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x));
+      setCartItems(cartItems.map((x) => (x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x)));
     } else {
       // if it does not exist in the cart we have to add it and when it is the first time added it is one
-      setCartItems([...cartItems, { ...product, qty: 1 }])
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
     }
   };
   const onRemove = (product) => {
     const exist = cartItems.find((x) => x.id === product.id);
     if (exist.qty === 1) {
       // With filter we return a boolean value
-      // if the item is not similar with product id it returns true so we leave it on the basket, 
+      // if the item is not similar with product id it returns true so we leave it on the basket,
       // otherwise we remove them
       setCartItems(cartItems.filter((x) => x.id !== product.id));
     } else {
       // the same ass the add function but -1
-      setCartItems(cartItems.map((x) => x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x));
+      setCartItems(cartItems.map((x) => (x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x)));
     }
   };
 
@@ -70,6 +68,7 @@ function App() {
             {/* I'm not sure how to implement the product that needs to be used by the drink.js */}
             <Route path="/Product" element={<Product cartItems={cartItems} onAdd={onAdd} onRemove={onRemove} />} />
             <Route path="*" element={<NoPage />} />
+            <Route path="drinkRating" element={<DrinkRating />}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/components/drinkRating/drinkRating.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { Box, Container, Typography, IconButton } from "@mui/material";
+import ThumbUpAltOutlinedIcon from "@mui/icons-material/ThumbUpAltOutlined"; // Import the outlined thumbs up icon
+import ThumbDownAltOutlinedIcon from "@mui/icons-material/ThumbDownAltOutlined"; // Import the outlined thumbs down icon
+
+const DrinkRating = () => {
+  const [choice, setChoice] = useState(null);
+
+  const handleThumbsUp = () => {
+    setChoice("liked");
+  };
+
+  const handleThumbsDown = () => {
+    setChoice("disliked");
+  };
+
+  const renderMessage = () => {
+    if (choice === "liked") {
+      return <Typography variant="h6">You liked your drink!</Typography>;
+    } else if (choice === "disliked") {
+      return <Typography variant="h6">You didn't like your drink.</Typography>;
+    }
+  };
+
+  return (
+    <Container>
+      <Box
+        sx={{
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
+        <Typography variant="h3" gutterBottom>
+          Thank you for your order
+        </Typography>
+        <Typography variant="h5" gutterBottom>
+          Did you enjoy your drink?
+        </Typography>
+        <Box>
+          <IconButton onClick={handleThumbsUp} color="primary" size="large">
+            <ThumbUpAltOutlinedIcon /> {/* Use the outlined thumbs up icon */}
+          </IconButton>
+          <IconButton onClick={handleThumbsDown} color="secondary" size="large">
+            <ThumbDownAltOutlinedIcon /> {/* Use the outlined thumbs down icon */}
+          </IconButton>
+        </Box>
+        {renderMessage()}
+      </Box>
+    </Container>
+  );
+};
+
+export default DrinkRating;
