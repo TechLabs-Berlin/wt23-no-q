2c69dc94fe04ef7abab72b361ac47c5afd79075b
SeannaMarie <seanna.coleman@wsu.edu>
2023-04-11 18:19:40 +0200

    Fixed issue with queue not updating
    added user name to queue

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -5,13 +5,14 @@ import { useUser } from "../../useData";
 export default function Profile() {
   const { usersArray } = useUser();
   const currentUser = usersArray[usersArray.length - 1]; // Get the last user in the array
-  const queueNumber = currentUser ? currentUser.number : "N/A";
+  const queueNumber = currentUser ? usersArray.length : "N/A";
+  const userName = currentUser ? currentUser.name : "N/A";
 
   return (
     <>
       <div className="orderPage">
         <div className="profile">
-          <h2>Hey, Your ID is and you are in the Queue! You are the number</h2>
+          <h2>{userName}, Your Order Number is:</h2>
 
           <div className="number-queue">#{queueNumber}</div>
 
