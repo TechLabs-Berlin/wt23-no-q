66a45bb236e815432e7a53ede6c7098c1de63b50
SeannaMarie <seanna.coleman@wsu.edu>
2023-04-12 13:34:01 +0200

    some mistakes from the merge were corrected

--- src/pages/drinks/drinks.js
+++ src/pages/drinks/drinks.js
@@ -1,10 +1,6 @@
 import React from "react";
 import Product from "./product";
-import Product from "./product";
 
-// to pass the data to components from the source we pass it as props
-export default function Drinks(props) {
-  const { products, onAdd } = props;
 // to pass the data to components from the source we pass it as props
 export default function Drinks(props) {
   const { products, onAdd } = props;
@@ -16,11 +12,9 @@ export default function Drinks(props) {
         {/* to find each element in the products and collect the data */}
         {products.map((product) => (
           // to pass the products to the Product component we have to call product={product} so in order to pass the product from the products(with the map) and pass it on
-          <Product key={product.id} product={product} onAdd={onAdd}>
-          </Product>
+          <Product key={product.id} product={product} onAdd={onAdd}></Product>
         ))}
       </div>
     </div>
-  )
+  );
 }
-
