533f812543191381e7bb0413ff156a3fbfcaca8e
PhaedraMaya <phaedra.sarpaki@gmail.com>
2023-04-14 02:21:34 +0300

    modifying welcome page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './home.css';
+import Tooltip from '@mui/material/Tooltip';
 
 
 
@@ -12,10 +13,15 @@ export default function Home() {
       <div className='container'>
 
         <Link to='/userform'>
-
-          <img className='logo-img' src='./images/newer_logo.png' alt='logo' />
-          {/* <button className='btn-welcome'>Welcome!</button> */}
-          {/* <button className='btn-welcome'>Welcome!</button> */}
+          {/* <Tooltip title='Hello you extrovert! Welcome in No-Q,click to get in the App!' arrow placement='top'> */}
+          <div className='image-container'>
+            <img className='logo-img' src='./images/newer_logo.png' alt='logo' />
+            <div className='text-overlay'>
+              'Hello you extrovert! Welcome in No-Q,click to get in the App!'
+            </div>
+          </div>
+          {/* 
+          </Tooltip> */}
         </Link>
         <h1 className='name-app'>NO-Q</h1>
 
